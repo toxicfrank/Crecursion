7e55c33616c1ca0e623c2776fd40e0797d6886b7
toxicfrank <ngugifrancis930@gmai.com>
2023-05-18 08:58:49 +0300

    practice

---
+++ 0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  function that prints a string, followed by a new line.
+ * @s: function parameter that points to a char data type
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif	
