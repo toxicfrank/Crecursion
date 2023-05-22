bebc7d3c842d3e0e43933ae0450d55a4fd6a0f7c
toxicfrank <ngugifrancis930@gmai.com>
2023-05-22 11:55:02 +0300

    practice

---
+++ 2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion -  function that returns the length of a string.
+ * @s: function parameter that points to a char data type
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		
+	}
+}
--- main.h
+++ main.h
@@ -4,8 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 void _puts_recursion(char *s);
 int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
 
 #endif	
